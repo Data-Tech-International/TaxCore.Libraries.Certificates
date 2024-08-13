34c003492f32be90cad8f0caaf356cf5755777ce
miljanhirsl <165056048+miljanhirsl@users.noreply.github.com>
2024-08-13 10:12:07 +0200

    Update CertRequestData.cs
    Add default values to properties

--- src/Certificates/CertRequestData.cs
+++ src/Certificates/CertRequestData.cs
@@ -4,27 +4,27 @@ namespace TaxCore.Libraries.Certificates
 {
     public class CertRequestData
     {
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
-        public string CommonName { get; set; }
+        public string CommonName { get; set; } = string.Empty;
 
-        public string DeviceSerialNumber { get; set; }
+        public string DeviceSerialNumber { get; set; } = string.Empty;
 
-        public string GivenName { get; set; }
+        public string GivenName { get; set; } = string.Empty;
 
-        public string SurName { get; set; }
+        public string SurName { get; set; } = string.Empty;
 
-        public string OrganizationUnit { get; set; }
+        public string OrganizationUnit { get; set; } = string.Empty;
 
-        public string Organization { get; set; }
+        public string Organization { get; set; } = string.Empty;
 
-        public string StreetAddress { get; set; }
+        public string StreetAddress { get; set; } = string.Empty;
 
-        public string Locality { get; set; }
+        public string Locality { get; set; } = string.Empty;
 
-        public string State { get; set; }
+        public string State { get; set; } = string.Empty;
 
-        public string DomainComponent { get; set; }
+        public string DomainComponent { get; set; } = string.Empty;
 
         public string Country { get; set; }
 
